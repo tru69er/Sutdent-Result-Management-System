4cc6ffac9d07834de94a0d2cdecfc5bc41929a54
tru69er <75154468+tru69er@users.noreply.github.com>
2022-05-19 08:49:25 +0530

    Add files via upload

---
+++ SimpleStudentResultm/session.php
@@ -0,0 +1,16 @@
+<?php
+   include('init.php');
+   session_start();
+   $conn = mysqli_connect('localhost', 'root', '', 'simplestresult');
+   $db = mysqli_select_db($conn,'simplestresult');
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($conn,"select userid from admin_login where userid= '$user_check'");
+   $row = mysqli_fetch_array($ses_sql);
+   
+   $login_session = $row['userid'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("Location:index.php");
+   }
+?>
